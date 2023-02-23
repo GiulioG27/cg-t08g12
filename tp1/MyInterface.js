c9f363fcb12547a652dd9415b9f8e094143e27fd
Giulio <up202211853@fe.up.pt>
2023-02-23 12:44:30 +0100

    First assignment

--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -19,6 +19,11 @@ export class MyInterface extends CGFinterface {
 
         //Checkbox element in GUI
         this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene,'displayDiamond').name('Display Diamond'); //how is this connected to the boolean value defined in myscene? by the name?
+        this.gui.add(this.scene,'displayTriangle').name('Display Triangle');
+        this.gui.add(this.scene,'displayParallelogram').name('Display Parallelogram');
+        this.gui.add(this.scene,'displayTriangleSmall').name('Display TriangleSmall');
+        this.gui.add(this.scene,'displayTriangleBig').name('Display TriangleBig');
 
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
--- tp1/MyParallelogram.js
+++ tp1/MyParallelogram.js
@@ -1,10 +1,10 @@
 import {CGFobject} from '../lib/CGF.js';
 /**
- * MyDiamond
+ * MyParallelogram
  * @constructor
  * @param scene - Reference to MyScene object
  */
-export class MyDiamond extends CGFobject {
+export class MyParallelogram extends CGFobject {
 	constructor(scene) {
 		super(scene);
 		this.initBuffers();
@@ -12,23 +12,21 @@ export class MyDiamond extends CGFobject {
 	
 	initBuffers() {
 		this.vertices = [
-			-1, 0, 0,	//0
-			0, -1, 0,	//1
-			0, 1, 0,	//2
-			1, 0, 0		//3
+			0, 0, 0,	//0
+			2, 0, 0,	//1
+			3, 1, 0,	//2
+            1, 1, 0     //3
 		];
-
 		//Counter-clockwise reference of vertices
 		this.indices = [
-			0, 1, 2,
-			1, 3, 2
+			0, 1, 3, //front
+            1, 2, 3,
+            3, 1, 0, //back
+            3, 2, 1
 		];
 
-		//The defined indices (and corresponding vertices)
-		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
 
 		this.initGLBuffers(); //passes information to WebGL
 	}
 }
-
